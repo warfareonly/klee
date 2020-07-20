c02d375d1447ce03c14bad4d0c91f78538802b49
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-07-20 23:02:51 +0800

    print byte read

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -2947,6 +2947,7 @@ void Executor::run(ExecutionState &initialState) {
           uint32_t value = 0;
           for (int i = 0; i < num_bytes; i++) {
             value += (obj.bytes[i]  << 8 * (num_bytes - i - 1) );
+            printf("\t\treading byte value=%d at i=%d and total=%d", obj.bytes[i], i, value );
           }
           var_map.insert(std::pair<char*, int>(obj.name, value));
           klee_warning("Reading Second Order Variable, name:%s, size:%d and value:%d",
