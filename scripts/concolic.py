7983a4d990928c75aff8750cfe4c494ea5bca421
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-07-23 02:17:49 +0800

    add concolic script

---
+++ scripts/concolic.py
@@ -0,0 +1,50 @@
+import sys
+import os
+import collections
+from six.moves import cStringIO
+from pysmt.smtlib.parser import SmtLibParser
+from pysmt.shortcuts import get_model, Solver
+
+
+def collect_symbolic_path(log_path, project_path):
+    ppc_list = collections.OrderedDict()
+    last_sym_path = ""
+    if os.path.exists(log_path):
+        source_path = ""
+        path_condition = ""
+        with open(log_path, 'r') as trace_file:
+            for line in trace_file:
+                if '[path:ppc]' in line:
+                    if project_path in line:
+                        source_path = str(line.replace("[path:ppc]", '')).split(" : ")[0]
+                        source_path = source_path.strip()
+                        source_path = os.path.abspath(source_path)
+                        path_condition = str(line.replace("[path:ppc]", '')).split(" : ")[1]
+                        continue
+                if source_path:
+                    if "(exit)" not in line:
+                        path_condition = path_condition + line
+                    else:
+                        if source_path not in ppc_list.keys():
+                            ppc_list[source_path] = list()
+                        ppc_list[source_path].append((path_condition))
+                        last_sym_path = path_condition
+                        source_path = ""
+                        path_condition = ""
+    # constraints['last-sym-path'] = last_sym_path
+    # print(constraints.keys())
+    return ppc_list, last_sym_path
+
+
+def analyse_symbolic_path(ppc_list, last_sym_path):
+    constraint_list = dict()
+    control_loc_list = ppc_list.keys()
+    control_loc_list.reverse()
+    for control_loc in control_loc_list:
+        ppc = ppc_list[control_loc]
+        parser = SmtLibParser()
+        script = parser.get_script(cStringIO(ppc))
+        formula = script.get_last_formula()
+        constraint = formula.arg(1)
+        print(control_loc, constraint)
+
